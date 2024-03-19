3ef4a2b20a97d1b0f3b890b3782818be7aa6fd09
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-19 10:20:46 +0200

    Add HashService and Validator for email or username. Add basic LoginController(not finished)

--- server-aspnet/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ server-aspnet/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+829569b1b3791d05cfa749f088ce185581960ba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d797a7bd89dada517070ce31904ee0eb077fb6d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
