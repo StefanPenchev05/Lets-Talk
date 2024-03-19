71c73e3b52d7084a28504cdedc3c1843b7cf4ea4
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-19 10:24:44 +0200

    Add Validations on LoginViewModel

--- server-aspnet/Models/ViewModels/LoginViewModel.cs
+++ server-aspnet/Models/ViewModels/LoginViewModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Server.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Username of Email is required")]
+        [StringLength(50, ErrorMessage = "Username or Email cannot be longer than 50 characters")]
         public string UsernameOrEmail { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be between 6 and 100 characters")]
         public string Password { get; set; }
     }
 }
