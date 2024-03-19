1139f5f8a0fc7a209d6100233c10932e7ac59672
StefanPenchev05 <comings.belts.0d@icloud.com>
2024-03-19 10:48:20 +0200

    Update AssemblyInfo version attribute

--- server-aspnet/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ server-aspnet/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c73e3b52d7084a28504cdedc3c1843b7cf4ea4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d59893e1bffdfb9de3290e2b3bf2830b549b9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
